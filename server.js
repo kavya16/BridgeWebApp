9f3a87e6a8e1b548abe6e62fae713ce27ab170f8
Kavya <kavyalakshmi2000@gmail.com>
2024-10-05 22:31:01 -0500

    remove dist

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname)));
 
 // API routes can be added here if needed, or proxy to bridgeAPI
 
 // The "catchall" handler: for any request that doesn't match, send back React's index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'), (err) => {
+  res.sendFile(path.join(__dirname, 'index.html'), (err) => {
     if (err) {
       console.error('Error sending index.html:', err);
       res.status(500).send('Server Error');
